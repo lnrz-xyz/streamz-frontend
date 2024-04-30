220a2981efb7be8b3af73e10bb7b1a5d2d1fe489
Benjamin Conn <benny@bennyc.io>
2024-04-30 12:34:06 -0400

    no mobile

--- app/components/LinkButton.js
+++ app/components/LinkButton.js
@@ -7,11 +7,11 @@ export default function LinkButton({ href, icon, text }) {
       <div className="group inline-flex items-center justify-center overflow-hidden rounded-full bg-background">
         <div
           className={`flex items-center group-hover:gap-2 justify-center group-hover:px-4 group-hover:py-2 text-white transition-all duration-500 ease-in-out
-                   w-12 h-12 group-hover:w-40 group-hover:rounded-lg`}>
+                   w-12 h-12 group-hover:sm:w-40 group-hover:rounded-lg`}>
           {icon}
           <span
             className="flex text-lg font-bold items-center justify-center overflow-hidden transition-all duration-500 ease-in-out
-                         max-w-0 group-hover:max-w-md">
+                         max-w-0 group-hover:sm:max-w-md">
             {text}
           </span>
         </div>
