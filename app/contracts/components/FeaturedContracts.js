4ebd518d98a63986e32b067dc575a88cc0a28522
Benjamin Conn <benny@bennyc.io>
2024-04-30 20:49:44 -0400

    more media kit

--- app/contracts/components/FeaturedContracts.js
+++ app/contracts/components/FeaturedContracts.js
@@ -2,6 +2,7 @@ import TotalScore from "./TotalScore"
 import FeaturedContract from "./FeaturedContract"
 import Link from "next/link"
 import CreateButtons from "./CreateButtons"
+import { ArrowUpRight } from "lucide-react"
 
 export default function FeaturedContracts({ featuredContracts }) {
   return (
@@ -10,12 +11,24 @@ export default function FeaturedContracts({ featuredContracts }) {
         <h2 className="text-center text-green-500 text-8xl font-bold">
           Collect on Streamz
         </h2>
-        <p className="text-center text-lg">
-          The more Streamz derivatives you collect, the more points you earn
-          towards the next airdrop. Collecting Sound NFTs will earn you an extra
-          boost in points. Create NFTs and add Streamz as a split on 0x to earn
-          even more points.
-        </p>
+        <div className="flex items-center flex-col gap-1">
+          <p className="text-center text-lg">
+            The more Streamz derivatives you collect, the more points you earn
+            towards the next airdrop. Collecting Sound NFTs will earn you an
+            extra boost in points. Create NFTs and add Streamz as a split on 0x
+            to earn even more points.
+          </p>
+          <div className="flex flex-row gap-2 p-4">
+            <a
+              href="https://www.figma.com/design/Pw3b5ePgjCh6fZadzYmzmi/Meme-Streamz?node-id=0%3A1&t=AIwhh2Jf1SUegn9H-1"
+              target="_blank"
+              rel="noreferrer"
+              className="underline text-white flex gap-1">
+              <p className="text-lg font-normal">Media Kit</p>
+              <ArrowUpRight size={16} />
+            </a>
+          </div>
+        </div>
 
         <CreateButtons />
       </div>
